b48ed6939840c1e9e2a5752a065aebcebbc62dd3
Prayash <prayashthapa15@gmail.com>
2026-07-19 16:18:03 +0545

    feat:: setup grpc server in c++

--- src/lib/FtpTextSource.cpp
+++ src/lib/FtpTextSource.cpp
@@ -1,119 +1,113 @@
 #include "FTPTextSource.h"
 #include <curl/curl.h>
-#include <sstream>
 #include <iostream>
+#include <sstream>
 
 tl::expected<std::string, std::string> test_ftp_text_source() {
-    std::cout << "FtpTextSource stub running. curl version: "
-              << curl_version() << "\n";
+  std::cout << "FtpTextSource stub running. curl version: " << curl_version()
+            << "\n";
 
-    if (curl_version() == nullptr) {
-        return tl::unexpected("Failed to get curl version");
-    }
+  if (curl_version() == nullptr) {
+    return tl::unexpected("Failed to get curl version");
+  }
 
-    return std::string("stub success");
+  return std::string("stub success");
 }
 
 namespace ftp_handler {
 
+tl::expected<FtpClient *, std::string>
+create_ftp_client(const std::string &host, const std::string &username,
+                  const std::string &password) {
 
-    tl::expected<FtpClient*, std::string> create_ftp_client(const std::string& host, const std::string& username, const std::string& password) {
+  CURL *curl = curl_easy_init();
+  if (!curl) {
 
-        CURL* curl = curl_easy_init();
-        if (!curl) {
+    return tl::unexpected("Failed to initialize curl handle");
+  }
 
-            return tl::unexpected("Failed to initialize curl handle");
+  const auto config = FtpConfig(host, username, password);
 
-        }
+  auto client = new FtpClient(curl, config);
 
-        const auto config = FtpConfig(
-            host,
-            username,
-            password
-            );
-
-
-        auto client = new FtpClient(curl , config);
-
-        return client;
-
-    }
-
-    tl::expected<void, std::string> destroy_curl_handler(const FtpClient* client) {
-
-        curl_easy_cleanup(client->curl_handler);
-        delete client;
-        return{};
-
-    }
-
-    void apply_common_opts(const FtpClient& client) {
-        curl_easy_setopt(client.curl_handler, CURLOPT_USERNAME, client.config.username.c_str());
-        curl_easy_setopt(client.curl_handler, CURLOPT_PASSWORD, client.config.password.c_str());
-        curl_easy_setopt(client.curl_handler, CURLOPT_CONNECTTIMEOUT, 10L);
-        curl_easy_setopt(client.curl_handler, CURLOPT_URL, client.config.host.c_str());
-    }
-
-    tl::expected<void, std::string> ftp_connect(const FtpClient& client) {
-
-        curl_easy_setopt(client.curl_handler, CURLOPT_URL, client.config.host.c_str());
-        curl_easy_setopt(client.curl_handler, CURLOPT_USERNAME, client.config.username.c_str());
-        curl_easy_setopt(client.curl_handler, CURLOPT_PASSWORD, client.config.password.c_str());
-        curl_easy_setopt(client.curl_handler, CURLOPT_CONNECT_ONLY, 0L);
-        curl_easy_setopt(client.curl_handler, CURLOPT_CONNECTTIMEOUT, 10l);
-
-        CURLcode res = curl_easy_perform(client.curl_handler);
-
-
-        if (res != CURLE_OK) {
-            return tl::unexpected("curl_easy_perform() failed");
-        }
-        return {};
-
-
-
-    }
-    size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
-        auto* out = static_cast<std::string*>(userdata);
-        out->append(ptr, size * nmemb);
-        return size * nmemb;
-    }
-
-    tl::expected<std::vector<ResourceInfo>, std::string> ftp_list_resources(const FtpClient& client) {
-
-        std::string listing;
-        apply_common_opts(client);
-        curl_easy_setopt(client.curl_handler, CURLOPT_DIRLISTONLY, 1L);
-        curl_easy_setopt(client.curl_handler, CURLOPT_WRITEFUNCTION, write_callback);
-        curl_easy_setopt(client.curl_handler, CURLOPT_WRITEDATA, &listing);
-
-        CURLcode res = curl_easy_perform(client.curl_handler);
-        if (res != CURLE_OK) {
-            return tl::unexpected(std::string("FTP list failed: ") + curl_easy_strerror(res));
-        }
-
-        std::vector<ResourceInfo> resources;
-
-        std::istringstream stream(listing);
-
-        std::string filename;
-        int count = 1;
-        while (std::getline(stream, filename)) {
-            if (!filename.empty() &&  filename.back() == '\r') {
-
-                filename.pop_back();
-
-            }
-
-            resources.push_back(ResourceInfo{std::to_string(count), filename});
-
-            count++;
-
-        }
-
-        return resources;
-
-
-    }
+  return client;
 }
 
+tl::expected<void, std::string> destroy_curl_handler(const FtpClient *client) {
+
+  curl_easy_cleanup(client->curl_handler);
+  delete client;
+  return {};
+}
+
+void apply_common_opts(const FtpClient &client) {
+  curl_easy_setopt(client.curl_handler, CURLOPT_USERNAME,
+                   client.config.username.c_str());
+  curl_easy_setopt(client.curl_handler, CURLOPT_PASSWORD,
+                   client.config.password.c_str());
+  curl_easy_setopt(client.curl_handler, CURLOPT_CONNECTTIMEOUT, 10L);
+}
+
+tl::expected<void, std::string> ftp_connect(const FtpClient &client) {
+
+  curl_easy_setopt(client.curl_handler, CURLOPT_URL,
+                   client.config.host.c_str());
+  curl_easy_setopt(client.curl_handler, CURLOPT_USERNAME,
+                   client.config.username.c_str());
+  curl_easy_setopt(client.curl_handler, CURLOPT_PASSWORD,
+                   client.config.password.c_str());
+  curl_easy_setopt(client.curl_handler, CURLOPT_CONNECT_ONLY, 0L);
+  curl_easy_setopt(client.curl_handler, CURLOPT_CONNECTTIMEOUT, 10l);
+
+  CURLcode res = curl_easy_perform(client.curl_handler);
+
+  if (res != CURLE_OK) {
+    return tl::unexpected("curl_easy_perform() failed");
+  }
+  return {};
+}
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+  auto *out = static_cast<std::string *>(userdata);
+  out->append(ptr, size * nmemb);
+  return size * nmemb;
+}
+
+tl::expected<std::vector<ResourceInfo>, std::string>
+ftp_list_resources(const FtpClient &client, std::string path) {
+
+  std::string listing;
+  apply_common_opts(client);
+  std::string url_path = client.config.host + path;
+  curl_easy_setopt(client.curl_handler, CURLOPT_URL, url_path.c_str());
+  curl_easy_setopt(client.curl_handler, CURLOPT_DIRLISTONLY, 1L);
+  curl_easy_setopt(client.curl_handler, CURLOPT_WRITEFUNCTION, write_callback);
+  curl_easy_setopt(client.curl_handler, CURLOPT_WRITEDATA, &listing);
+  curl_easy_setopt(client.curl_handler, CURLOPT_VERBOSE, 1L);
+  std::cout<<"url path" <<url_path<<std::endl;
+
+  CURLcode res = curl_easy_perform(client.curl_handler);
+  if (res != CURLE_OK) {
+    return tl::unexpected(std::string("FTP list failed: ") +
+                          curl_easy_strerror(res));
+  }
+
+  std::vector<ResourceInfo> resources;
+
+  std::istringstream stream(listing);
+
+  std::string filename;
+  int count = 1;
+  while (std::getline(stream, filename)) {
+    if (!filename.empty() && filename.back() == '\r') {
+
+      filename.pop_back();
+    }
+
+    resources.push_back(ResourceInfo{std::to_string(count), filename});
+
+    count++;
+  }
+
+  return resources;
+}
+} // namespace ftp_handler
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,64 @@
 #include "FTPTextSource.h"
 #include <iostream>
 
+#include "Anime.grpc.pb.h"
 #include "lib/config/config.h"
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+
+int ftp_main() {
+  std::string host = config::get_env_or("FTP_HOST");
+  std::string username = config::get_env_or("FTP_USER");
+  std::string password = config::get_env_or("FTP_PASS");
+  std::string current_path = "//anime/";
+
+  ftp_handler::FtpConfig config{host, username, password};
+
+  auto client = ftp_handler::create_ftp_client(host, username, password);
+
+  if (!client) {
+    std::cerr << "Error creating ftp client" << client.error() << std::endl;
+  }
+
+  auto result = ftp_handler::ftp_connect(*client.value());
+
+  auto resources =
+      ftp_handler::ftp_list_resources(*client.value(), current_path);
+
+  if (!resources) {
+    std::cerr << "Error creating ftp resources" << resources.error()
+              << std::endl;
+  }
+
+  for (const auto &resource : *resources) {
+    std::cout << resource.name << std::endl;
+  }
+}
+
+class AnimeServiceImpl final : public anime::AnimeService::Service {
+  grpc::Status Ping(grpc::ServerContext *context,
+                    const anime::PingRequest *request,
+                    anime::PongReply *reply) override {
+    std::cout << "Received: " << request->message() << std::endl;
+    reply->set_message("Pong: " + request->message());
+    return grpc::Status::OK;
+  }
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:50051");
+  AnimeServiceImpl service;
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  grpc::ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+
+  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
 
 int main() {
-    std::string host     = config::get_env_or("FTP_HOST");
-    std::string username = config::get_env_or("FTP_USER");
-    std::string password = config::get_env_or("FTP_PASS");
-
-    ftp_handler::FtpConfig config{
-        host,
-        username,
-        password
-    };
-
-    auto client = ftp_handler::create_ftp_client(
-        host,
-        username,
-        password
-        );
-
-    if (!client) {
-        std::cerr << "Error creating ftp client" <<client.error() <<std::endl;
-    }
-
-    auto result = ftp_handler::ftp_connect(
-        *client.value()
-        );
-
-    auto resources = ftp_handler::ftp_list_resources(
-        *client.value()
-        );
-
-    if (!resources) {
-        std::cerr << "Error creating ftp resources" << resources.error()<<std::endl;
-    }
-
-    for (const auto& resource : *resources) {
-        std::cout << resource.name << std::endl;
-    }
-
-
-
-
-
+  RunServer();
+  return 0;
 }
