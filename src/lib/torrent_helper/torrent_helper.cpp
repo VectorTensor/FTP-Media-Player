a470dc105e2c5e64b29f4d6408a351b79dc4265a
Prayash Thapa <prayashthapa15@gmail.com>
2026-09-20 17:37:46 +0545

    add logs

--- src/lib/torrent_helper/torrent_helper.cpp
+++ src/lib/torrent_helper/torrent_helper.cpp
@@ -9,6 +9,7 @@ lt::torrent_handle start_torrent_download(lt::session& session, const std::strin
     std::ostringstream oss;
     const std::string base_name = config::get_env_or("CFG_BASE_DOWNLOAD_PATH", "./");
     oss <<base_name<<download_path;
+    std::cout<<"downloadd to : "<<oss.str()<<std::endl;
     atp.save_path = oss.str();
     atp.flags = atp.flags
               & ~lt::torrent_flags::auto_managed   // don't let libtorrent's queue manage it
